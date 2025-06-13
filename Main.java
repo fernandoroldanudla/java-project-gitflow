739655f6c75b94c3aa06cc1426ccbf7d72fc70d0
fernandoroldanudla <fernando.roldan@udla.edu.ec>
2025-06-13 01:08:25 +0000

    Preparar versión 1.0.0

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Clase de Calidad de Software"));
+        System.out.println("Versión 1.0.0 lista para producción.");
     }
 }
